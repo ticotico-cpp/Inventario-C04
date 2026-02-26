70d9230d54519fed7b7c694963ca46dcc3a59388
ticotico-cpp <tiiiiiiiiiiagoabreu@gmail.com>
2026-02-26 17:39:35 -0300

    Primeira Entrega

---
+++ main.cpp
@@ -0,0 +1,54 @@
+/*
+Tiago Santiago Bustamante Abreu - 2336
+Lucas Vasconcelos Castro - 747
+Lucas Caixeta Generoso - 669
+Lucas Kazue da Silva Hokari - 2309
+Luigi Galina Di Lazzaro - 2317
+*/
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    char cmd;
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    string inicio = "Olá! Bem vindo ao sistema de gerenciamento de itens mágicos! Escolha uma opção:\na. Inserir item;\nb. Cadastrar similaridade entre itens;\nc. Buscar itens similares;\nd. Verificar a existência de um item;\ne. Listar itens (ordem alfabética de nome);\nf. Listar itens (ordem decrescente de raridade);\ng. Contar itens com mesma propriedade mágica;\nh. Remover itens menos raros;\nx. Sair do programa.\nEscolha: ";
+    while(cout << inicio && cin >> cmd) {
+        switch (cmd) {
+            case 'a':
+                cout << endl << "Função em construção" << endl << endl;
+                break;
+            case 'b':
+                cout << endl << "Função em construção" << endl << endl;
+                break;
+            case 'c':
+                cout << endl << "Função em construção" << endl << endl;
+                break;
+            case 'd':
+                cout << endl << "Função em construção" << endl << endl;
+                break;
+            case 'e':
+                cout << endl << "Função em construção" << endl << endl;
+                break;
+            case 'f':
+                cout << endl << "Função em construção" << endl << endl;
+                break;
+            case 'g':
+                cout << endl << "Função em construção" << endl << endl;
+                break;
+            case 'h':
+                cout << endl << "Função em construção" << endl << endl;
+                break;
+            case 'x':
+                cout << endl << "Saindo do programa..." << endl << endl;
+                return 0;
+            default:
+                cout << endl << "Digite um comando válido!" << endl << endl;
+                break;
+        }
+    }
+    return 0;
+}
